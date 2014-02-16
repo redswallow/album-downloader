e6152340773ed31cbaf270048614ae49319c4703
redswallow <redswallowjysc@gmail.com>
2014-02-15 18:53:26 -0800

    parse from the homepage of album
    1. add rules: Rule(SgmlLinkExtractor(allow =
    'photos/album/\d+/[\?start=\d+]?'), callback = 'parse_image', follow =
    True)
    2. set start_urls in __init__
    3. modify function parse to parse_image

--- bot/spiders/douban_image_spider.py
+++ bot/spiders/douban_image_spider.py
@@ -1,16 +1,23 @@
-from scrapy.spider import Spider
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import Selector
 from bot.items import ImageItem
 
-class DoubanImageSpider(Spider):
+class DoubanImageSpider(CrawlSpider):
     name = "douban_image_spider"
     allowed_domains = ["douban.com"]
-    start_urls = ['http://www.douban.com/photos/album/117515388']
+    start_urls = []
+    rules = [
+            Rule(SgmlLinkExtractor(allow = 'photos/album/\d+/[\?start=\d+]?'), callback = 'parse_image', follow = True)
+    ]
 
-    def __init__(self):
-        pass
+    def __init__(self, username = 'redswallow', *args, **kwargs):
+        super(DoubanImageSpider, self).__init__(*args, **kwargs)
+        self.start_urls = [
+                "http://www.douban.com/people/%s/photos" % username
+        ]
 
-    def parse(self, response):
+    def parse_image(self, response):
         sel = Selector(response)
         photolst = sel.xpath('//div[@class="photolst clearfix"]/div')
         items = []
